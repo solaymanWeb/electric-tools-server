4e602ac92488ed8d6899d519f18b37c325ff89f4
solayman1e <solaymanweb27@gmail.comm>
2022-05-27 19:36:12 +0600

    all tools get client site

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middle ware
 app.use(cors());
@@ -16,7 +16,33 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.r1x0s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
 
+    try{
+        await client.connect();
+        const toolsCollection = client.db('electric-tools').collection('tools')
+
+        app.get('/tools', async(req, res)=>{
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools)
+        })
+
+        app.get('/tools/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool)
+
+
+        })
+
+    }finally{
+
+    }
+}
+run().catch(console.dir);
 
 
 // client.connect(err => {
