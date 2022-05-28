e896828dbba1b4e12de224e81ebee092d101ecca
solayman1e <solaymanweb27@gmail.comm>
2022-05-28 19:57:27 +0600

    delete api

--- index.js
+++ index.js
@@ -77,6 +77,15 @@ async function run(){
             res.send(result)
         })
 
+        //delet api
+        
+        app.delete('/purchase/:id', async(req, res)=>{
+            const id =req.params.id;
+            const query = {_id: ObjectId(id) }
+            const result = await purchaseCollection.deleteOne(query)
+            res.send(result);
+        })
+
     }finally{
 
     }
